bd9e304768151f75b16b1aa2ee64019fe3518fae
yyyort <itpahilgadev@gmail.com>
2025-01-04 20:29:34 +0800

    web: patch env config

--- web/next.config.ts
+++ web/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     env: {
-        SERVER_URL: 'http://localhost:4000/api'
+        SERVER_URL: process.env.SERVER_URL,
     }
 };
 
