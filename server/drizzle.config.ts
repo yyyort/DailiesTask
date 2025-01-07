424dca1239156d7f5fc6634cc1dea92b78f312b2
yyyort <itpahilgadev@gmail.com>
2025-01-07 14:06:04 +0800

    server: update db config to accomodate neon

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -3,13 +3,15 @@ import { defineConfig } from 'drizzle-kit';
 import dotenv from 'dotenv';
 
 dotenv.config();
+const node_env = process.env.NODE_ENV;
+const db_url = node_env === 'dev' ? process.env.DEV_DATABASE_URL : process.env.DATABASE_URL;
 
 export default defineConfig({
   out: './drizzle',
   schema: './src/db/schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
-    url: String(process.env.DATABASE_URL),
+    url: String(db_url),
   },
 });
 
